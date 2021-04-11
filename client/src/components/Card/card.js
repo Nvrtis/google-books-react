168d6ef7892426b892cf47fea3d0f916102db164
Nicholas Vrtis <nvav91@gmail.com>
2021-04-11 23:28:01 +0200

    working map

--- client/src/components/Card/card.js
+++ client/src/components/Card/card.js
@@ -3,16 +3,18 @@ import "./style.css"
 
 
 // deconstruct props and makes the cards for projects
-const Card = ({ title, link, repoLink, desc,img}) => {
-
+const Card = ({title,img, authors, link}) => {
+  // console.log(props.volumeInfo.imageLinks.smallThumbnail)
+  // console.log(props.volumeInfo.imageLinks.thumbnail)
+console.log(title)
   return (
     <div className="card">
       <div className="card-body">
-        <img src={img} className="img-fluid img" alt="project-image"/>
+        <img src={img} className="img-fluid img" alt={title}/>
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">{desc}</p>
-        <a href={link} className="btn btn-primary">Link to live page</a>
-        <a href={repoLink} className="btn btn-primary">Link to repo page</a>
+        <p className="card-text">Authors: {authors}</p>
+        <a href={link} className="btn btn-primary">Link to page</a>
+
       </div>
     </div>
   )
--- client/src/components/col/col.js
+++ client/src/components/col/col.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Col = ({ className, ...props }) => (
-  <div className={`col ${className}`} {...props} />
+  <div className={`col-3 ${className}`} {...props} />
 )
 
 export default Col
